9d0d3bcb9e4afe156ae5f177d64be1ea8efe93e4
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-02-07 13:02:27 +0100

    Initial commit. Add code from prototyping phase.

---
+++ main.py
@@ -0,0 +1,105 @@
+"""
+The streamlit implementation of SUSI (Simple UI for Simulation Input).
+
+Run this with
+`python -m streamlit run main.py`
+after having installed dependencies with
+`pip install streamlit streamlitflow`
+"""
+import streamlit as st
+from streamlit_flow import streamlit_flow
+from streamlit_flow.elements import StreamlitFlowNode
+from streamlit_flow.state import StreamlitFlowState
+from streamlit_flow.layouts import ManualLayout
+
+def check_state():
+    """Ensures the current state is attached to the simulation state and creates it if not."""
+    if 'current_state' not in st.session_state:
+        # for some reason initialising with an empty node list causes the state to not
+        # recognize when nodes are added. as a workaround we initialise with a hidden
+        # dummy node and ignore it in the output
+        st.session_state.current_state = StreamlitFlowState(
+            [StreamlitFlowNode("dummy", (0,0), {"content": ""}, hidden=True)],
+            []
+        )
+
+def add_node(new_node):
+    """Add the given node to the node list.
+
+    # Args:
+    -`new_node:StreamlitFlowNode`: The node to add
+    """
+    check_state()
+    st.session_state.current_state.nodes.append(new_node)
+
+def nr_of_nodes(segment, nodes):
+    """The number of nodes with the given segment occuring in the UAC.
+    
+    # Args:
+    -`segment:str`: The segment to look for
+    -`nodes:List<StreamlitFlowNode>`: The nodes to search through
+    # Returns:
+    -`int`: The number of nodes with the given segment in their UA
+    """
+    return len([n for n in nodes if segment in n.id])
+
+def lpad(to_pad, to_len, pad_char="0"):
+    """Left-pads the given string.
+
+    Doesn't handle edge cases very well.
+
+    # Args:
+    -`to_pad:str`: The string to pad
+    -`to_len`: Length to which to pad
+    -`pad_char`: The character used for padding
+    # Returns:
+    -`str`: The padded string
+    """
+    return (str(pad_char) * int(to_len - len(to_pad))) + to_pad
+
+def main():
+    """Entry point to the streamlit process."""
+    check_state()
+    st.set_page_config(layout="wide")
+
+    st.title('Simple UI for Simulation Input')
+
+    with st.sidebar:
+        prefix = st.text_input("UAC prefix", "TST")
+
+        st.markdown("Special")
+
+        if st.button("Add bus"):
+            uac = prefix + "_BUS_" + lpad(
+                str(nr_of_nodes("_BUS_", st.session_state.current_state.nodes) + 1), 2, "0"
+            )
+            new_node = StreamlitFlowNode(
+                uac,
+                (0, 0),
+                {
+                    'content': uac,
+                    "foo": "bar"
+                },
+                'default', 'right', 'left',
+                deletable=True
+            )
+            add_node(new_node)
+            st.rerun()
+
+    st.session_state.current_state = streamlit_flow(
+        'energy_system', 
+        st.session_state.current_state,
+        layout=ManualLayout(),
+        fit_view=True,
+        enable_node_menu=True,
+        enable_edge_menu=True,
+        enable_pane_menu=True,
+        get_edge_on_click=True,
+        get_node_on_click=True,
+        show_minimap=True,
+        hide_watermark=True,
+        allow_new_edges=True,
+        min_zoom=0.1
+    )
+
+main()
