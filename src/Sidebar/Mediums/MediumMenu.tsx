87a5b652efa188b25b6e3905e4fe2857c16b97c2
swerum <swerum.subs@gmail.com>
2026-04-17 13:49:30 +0200

    feat: add Button to add a new medium

--- src/Sidebar/Mediums/MediumMenu.tsx
+++ src/Sidebar/Mediums/MediumMenu.tsx
@@ -1,8 +1,14 @@
 import { useContext } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Button } from 'react-bootstrap';
 import type { Medium } from '../../NodeDataStructures/Medium';
 import MediumInputWidget from './MediumInputWidget';
 import { AppContext } from '../../Reactflow-Components/AppContext';
+
+const getRandomColor = () => {
+	return `#${Math.floor(Math.random() * 0x1000000)
+		.toString(16)
+		.padStart(6, '0')}`;
+};
 
 const MediumMenu = () => {
 	const context = useContext(AppContext);
@@ -21,6 +27,16 @@ const MediumMenu = () => {
 		const newMediums = mediums.filter((m) => m.key !== key);
 		setMediums(newMediums);
 	};
+	const addMedium = () => {
+		const newMedium: Medium = {
+			name: 'm_untitled',
+			key: `m_${new Date().getTime()}`,
+			color: getRandomColor(),
+		};
+		const newMediums = Object.assign([], mediums);
+		newMediums.push(newMedium);
+		setMediums(newMediums);
+	};
 
 	return (
 		<>
@@ -37,6 +53,9 @@ const MediumMenu = () => {
 							/>
 						)
 				)}
+				<Button variant="secondary" size="sm" onClick={addMedium}>
+					<i className="bi bi-plus-circle" /> Add new Medium
+				</Button>
 			</Col>
 		</>
 	);
