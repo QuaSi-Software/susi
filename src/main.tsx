c6971611be9e4dc2b7dc6e4ba37e36f0c7e11495
swerum <swerum.subs@gmail.com>
2026-06-24 11:35:50 +0200

    fix: scrolling with date widget open error

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './CSS/index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<PrimeReactProvider>
+			<App />
+		</PrimeReactProvider>
 	</StrictMode>
 );
-
