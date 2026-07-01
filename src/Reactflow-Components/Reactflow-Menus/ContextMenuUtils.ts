70e5e682be46b311375f83df43cc5ae07a2ed5fa
swerum <swerum.subs@gmail.com>
2026-07-01 15:26:46 +0200

    refactor: move duplicate node logi to ContextMenuUtils

--- src/Reactflow-Components/Reactflow-Menus/ContextMenuUtils.ts
+++ src/Reactflow-Components/Reactflow-Menus/ContextMenuUtils.ts
@@ -1,7 +1,8 @@
 import type { Dispatch, SetStateAction } from 'react';
-import type { SusiNode } from '../../NodeDataStructures/Nodes/SusiNode';
+import { deepCloneNode, type SusiNode } from '../../NodeDataStructures/Nodes/SusiNode';
 import type { SusiEdge } from '../../NodeDataStructures/Edges/SusiEdge';
 import { updateBusDataOnNodeDelete } from '../../NodeDataStructures/Bus/BusDataUtils';
+import BusData from '../../NodeDataStructures/Bus/BusData';
 
 function deleteNode(
 	node: SusiNode,
@@ -22,5 +23,55 @@ function deleteNode(
 		});
 	}
 }
+/**
+ * Seperate the node name into the text part and the number at the end,
+ * And increase the number at the end until it's a unique name
+ * @param {string} name The name of the node being duplicated
+ * @param {*} nodes A list of all the nodes in the scene
+ * @returns {string} The name for the duplicated node
+ */
+const findNameForDuplicate = (name: string, nodes: SusiNode[]) => {
+	// divide the node name into a string part nameBase and whatever number is at the end of the name
+	// if there is no number, the nameBase will just be the name and the new number will be 1
+	const match = name.match(/^(.*?)(\d+)$/);
+	let nameBase = match ? match[1] : name;
+	let number = match ? parseInt(match[2]) + 1 : 1;
+	// increase number until the name 'nameBase + number' (with and without 0 padding) is not taken
+	while (nodes.find((node: SusiNode) => nameMatches(node.data.content, nameBase, number))) {
+		number++;
+	}
+	return getPaddedName(nameBase, number);
 
-export { deleteNode };
+	/** Get the node name where the number is zero padded to >2 digits */
+	function getPaddedName(nameBase: string, number: number) {
+		let paddedNumber = (number < 10 ? '0' : '') + number;
+		return nameBase + paddedNumber;
+	}
+	/** Check if the name matches either the regular or padded version of nameBase+number */
+	function nameMatches(nameToCheck: string, nameBase: string, number: number) {
+		return nameToCheck === getPaddedName(nameBase, number) || nameToCheck === nameBase + number;
+	}
+};
+
+function duplicateNode(nodeID: string, setNodes: Dispatch<SetStateAction<SusiNode[]>>) {
+	// duplicate node object
+	setNodes((nodes) => {
+		const nodeToDuplicate: SusiNode | undefined = nodes.find((node) => node.id === nodeID);
+		console.assert(nodeToDuplicate != undefined, `Trying to duplicate a node that doesn't exist`);
+		if (!nodeToDuplicate) return nodes;
+		const duplicateNode: SusiNode = deepCloneNode(nodeToDuplicate);
+		// move node towards bottom right and give it a unique ID
+		duplicateNode.position.x += 20;
+		duplicateNode.position.y += 20;
+		duplicateNode.id = nodeToDuplicate.id + '_' + new Date().getTime();
+		let isBus = duplicateNode.data.componentType.toLowerCase() === 'bus';
+		duplicateNode.data.busData = isBus ? new BusData() : null;
+		duplicateNode.data.content = findNameForDuplicate(nodeToDuplicate.data.content, nodes);
+		duplicateNode.selected = false;
+		// update list of nodes
+		let updatedNodes = [...nodes, duplicateNode];
+		return updatedNodes;
+	});
+}
+
+export { deleteNode, duplicateNode };
--- src/Reactflow-Components/Reactflow-Menus/NodeContextMenu.tsx
+++ src/Reactflow-Components/Reactflow-Menus/NodeContextMenu.tsx
@@ -3,13 +3,12 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/esm/ButtonGroup';
 
 import type { MenuPosition } from './Menus';
-import { deepCloneNode, type SusiNode } from '../../NodeDataStructures/Nodes/SusiNode';
+import { type SusiNode } from '../../NodeDataStructures/Nodes/SusiNode';
 import EditNodeModal from './EditNodeModal';
-import BusData from '../../NodeDataStructures/Bus/BusData';
 import type { SusiEdge } from '../../NodeDataStructures/Edges/SusiEdge';
 import _ from 'lodash';
 import { AppContext } from '../../AppContext';
-import { deleteNode } from './ContextMenuUtils';
+import { deleteNode, duplicateNode } from './ContextMenuUtils';
 
 interface NodeContextMenuInput {
 	nodeContextMenu: NodeContextMenuData | null;
@@ -63,57 +62,12 @@ const NodeContextMenu = ({
 	};
 
 	/**
-	 * Seperate the node name into the text part and the number at the end,
-	 * And increase the number at the end until it's a unique name
-	 * @param {string} name The name of the node being duplicated
-	 * @param {*} nodes A list of all the nodes in the scene
-	 * @returns {string} The name for the duplicated node
-	 */
-	const findNameForDuplicate = (name: string, nodes: SusiNode[]) => {
-		// divide the node name into a string part nameBase and whatever number is at the end of the name
-		// if there is no number, the nameBase will just be the name and the new number will be 1
-		const match = name.match(/^(.*?)(\d+)$/);
-		let nameBase = match ? match[1] : name;
-		let number = match ? parseInt(match[2]) + 1 : 1;
-		// increase number until the name 'nameBase + number' (with and without 0 padding) is not taken
-		while (nodes.find((node: SusiNode) => nameMatches(node.data.content, nameBase, number))) {
-			number++;
-		}
-		return getPaddedName(nameBase, number);
-
-		/** Get the node name where the number is zero padded to >2 digits */
-		function getPaddedName(nameBase: string, number: number) {
-			let paddedNumber = (number < 10 ? '0' : '') + number;
-			return nameBase + paddedNumber;
-		}
-		/** Check if the name matches either the regular or padded version of nameBase+number */
-		function nameMatches(nameToCheck: string, nameBase: string, number: number) {
-			return nameToCheck === getPaddedName(nameBase, number) || nameToCheck === nameBase + number;
-		}
-	};
-
-	/**
 	 * Duplicate the selected node. Move the duplicated node towards the bottom right.
 	 * Give the duplicated node a unique ID and name.
 	 */
 	const handleDuplicateNode = () => {
 		if (!nodeContextMenu) return;
-		// duplicate node object
-		const nodeToDuplicate: SusiNode | undefined = nodes.find((node) => node.id === nodeContextMenu.node.id);
-		console.assert(nodeToDuplicate != undefined, `Trying to duplicate a node that doesn't exist`);
-		if (!nodeToDuplicate) return;
-		const duplicateNode: SusiNode = deepCloneNode(nodeToDuplicate);
-		// move node towards bottom right and give it a unique ID
-		duplicateNode.position.x += 20;
-		duplicateNode.position.y += 20;
-		duplicateNode.id = nodeToDuplicate.id + '_' + new Date().getTime();
-		let isBus = duplicateNode.data.componentType.toLowerCase() === 'bus';
-		duplicateNode.data.busData = isBus ? new BusData() : null;
-		duplicateNode.data.content = findNameForDuplicate(nodeToDuplicate.data.content, nodes);
-		duplicateNode.selected = false;
-		// update list of nodes
-		let updatedNodes = [...nodes, duplicateNode];
-		setNodes(updatedNodes);
+		duplicateNode(nodeContextMenu.node.id, setNodes);
 		setNodeContextMenu(null);
 		setCheckState(true);
 	};
