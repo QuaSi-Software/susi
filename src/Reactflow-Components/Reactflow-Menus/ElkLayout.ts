2bbba53bc18c5ac01a639fed05611ce536dc3f71
swerum <swerum.subs@gmail.com>
2026-07-21 10:40:02 +0200

    feat: pass elkjs a hierarchy of nodes to fix layoutting subflows

--- src/Reactflow-Components/Reactflow-Menus/ElkLayout.ts
+++ src/Reactflow-Components/Reactflow-Menus/ElkLayout.ts
@@ -1,47 +1,72 @@
 // import Elk from 'elkjs/lib/elk.bundled.js';
-import { deepCloneNodes, type SusiNode } from '../../NodeDataStructures/Nodes/SusiNode';
+import { type SusiNode } from '../../NodeDataStructures/Nodes/SusiNode';
 import type { SusiEdge } from '../../NodeDataStructures/Edges/SusiEdge';
+import type { ElkNode } from 'elkjs';
 
-const createElkGraphLayout = async (graphNodes: Array<SusiNode>, graphEdges: Array<SusiEdge>) => {
+type GraphNode = ElkNode & SusiNode;
+
+const createElkGraphLayout = async (graphNodes: Array<SusiNode>, graphEdges: Array<SusiEdge>): Promise<SusiNode[]> => {
 	/** Set up Layout options */
 	// const elk = new Elk({
 	const elk = new (await import('elkjs')).default({
 		defaultLayoutOptions: {
 			'elk.algorithm': 'layered',
 			'elk.direction': 'RIGHT',
-			'elk.spacing.nodeNode': '75',
-			'elk.layered.spacing.nodeNodeBetweenLayers': '75',
-			'elk.layered.considerModelOrder.strategy': 'NODES_AND_EDGES',
+			'elk.hierarchyHandling': 'INCLUDE_CHILDREN',
+			'elk.layered.crossingMinimization.strategy': 'LAYER_SWEEP',
+			'elk.layered.nodePlacement.strategy': 'NETWORK_SIMPLEX',
+			'elk.layered.considerModelOrder': 'NODES_AND_EDGES',
+			'elk.edgeRouting': 'ORTHOGONAL',
+			'elk.spacing.nodeNode': '40',
+			'elk.layered.spacing.nodeNodeBetweenLayers': '60',
 		},
 	});
 
 	/** add sources and targets to edges, make a deep copy of the nodes */
-	const nodes: Array<SusiNode> = deepCloneNodes(graphNodes);
 	const edges = graphEdges.map((e) => ({ ...e, sources: [e.source], targets: [e.target] }));
+	const nodes: GraphNode[] = graphNodes.map((n) => ({
+		...n,
+		id: n.id,
+		width: n.measured?.width ?? 75,
+		height: n.measured?.height ?? 75,
+		type: n.type ?? 'default',
+		parentId: n.parentId,
+	}));
+	const children = nodes.filter((n) => n.type === 'group' || !n.parentId);
+	children.forEach((parentNode) => {
+		if (parentNode.type !== 'group') return;
+		const groupChildren = nodes.filter((n) => n.parentId === parentNode.id);
+		parentNode.children = groupChildren;
+	});
 
 	/** calculate new layout */
 	const newGraph = await elk.layout({
 		id: 'root',
-		children: nodes.map((node: SusiNode) => ({
-			...node,
-			width: node.measured?.width,
-			height: node.measured?.height,
-		})),
+		children,
 		edges: edges,
 	});
 
+	/** turn the child graph into an array again */
+	const formattedNodesWithoutParents: ElkNode[] = newGraph.children ?? [];
+	let formattedNodes: ElkNode[] = [];
+	formattedNodesWithoutParents.forEach((node) => {
+		if (!node.children) return;
+		formattedNodes = formattedNodes.concat(node.children);
+	});
+	formattedNodes = formattedNodes.concat(formattedNodesWithoutParents);
+
 	/** set the position of the nodes using the calculated graph */
 	nodes.forEach((node) => {
-		const newLayoutNode = newGraph.children?.find((n) => n.id === node.id);
+		const newLayoutNode = formattedNodes.find((n) => n.id === node.id);
 		if (newLayoutNode?.x && newLayoutNode?.y && newLayoutNode?.width && newLayoutNode?.height) {
 			node.position = {
-				x: newLayoutNode.x,
-				y: newLayoutNode.y,
+				x: newLayoutNode.x + newLayoutNode.width / 2,
+				y: newLayoutNode.y + newLayoutNode.height / 2,
 			};
 		}
-		return node;
+		return node as SusiNode;
 	});
-	return nodes;
+	return nodes as SusiNode[];
 };
 
 export default createElkGraphLayout;
